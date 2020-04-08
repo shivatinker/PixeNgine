b2679cec1ae17cb68fa1e5e791311d59c70bf24b
Andrii Zinoviev <zinoviev@stud.onu.edu.ua>
2020-04-09 01:25:41 +0300

    Initial Commit

---
+++ PixeNgine/PixeNgine.h
@@ -0,0 +1,19 @@
+//
+//  PixeNgine.h
+//  PixeNgine
+//
+//  Created by Andrii Zinoviev on 09.04.2020.
+//  Copyright © 2020 Andrii Zinoviev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PixeNgine.
+FOUNDATION_EXPORT double PixeNgineVersionNumber;
+
+//! Project version string for PixeNgine.
+FOUNDATION_EXPORT const unsigned char PixeNgineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixeNgine/PublicHeader.h>
+
+
